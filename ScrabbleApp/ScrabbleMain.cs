a74094713beedc73ff8adec55a2b5be86a50595e
Gareth James <81640442+gareth5james@users.noreply.github.com>
2023-03-07 19:53:25 +0000

    Add App
    Upload App

---
+++ ScrabbleApp/ScrabbleMain.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+ScrabbleScore scorer = new ScrabbleScore();
+
+Console.WriteLine("Enter a word to score (enter '-' to exit): ");
+
+string word = Console.ReadLine();
+
+while (word != "-")
+{
+    if (word == "")
+    {
+        Console.WriteLine("Please enter a word, or '-' to exit: ");
+
+        word = Console.ReadLine();
+    }
+    else if (word.Contains(" "))
+    {
+        Console.WriteLine("No spaces please. Please enter a word, or '-' to exit: ");
+
+        word = Console.ReadLine();
+    } else
+    {
+        Console.WriteLine("Score is: " + scorer.GetScrabbleScore(word));
+
+        Console.WriteLine("Enter a word to score (enter '-' to exit): ");
+
+        word = Console.ReadLine();
+    }
+}
+
+
---
+++ ScrabbleApp/ScrabbleScore.cs
@@ -0,0 +1,39 @@
+﻿ class ScrabbleScore
+    {
+        Dictionary<int, string> PointScores = new Dictionary<int, string>
+        {
+            { 1, "AEIOULNRST" },
+            { 2, "DG" },
+            { 3, "BCMP" },
+            { 4, "FHVWY" },
+            { 5, "K" },
+            { 8, "JX" },
+            { 10, "QZ" },
+        };
+    
+        const string OnePointScore = "AEIOULNRST";
+        const string TwoPointScore = "DG";
+        const string ThreePointScore = "BCMP";
+        const string FourPointScore = "FHVWY";
+        const string FivePointScore = "K";
+        const string EightPointScore = "JX";
+        const string TenPointScore = "QZ";
+
+        public int GetScrabbleScore(string word)
+        {
+            int wordScore = 0;
+
+            foreach(char c in word.ToUpper())
+            {
+                foreach(int score in PointScores.Keys)
+            {
+                if (PointScores[score].Contains(c))
+                {
+                    wordScore += score;
+                }
+            }
+            }
+
+            return wordScore;
+        }
+    }
